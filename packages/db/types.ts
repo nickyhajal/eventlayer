e682c2b327a4c558fadcffb5c8c49d145ee68d1f
Nicky Hajal <nhajal@gmail.com>
2024-04-01 13:00:46 +0200

    chore: attendeestore type

--- packages/db/types.ts
+++ packages/db/types.ts
@@ -1,2 +1,15 @@
+import { Media } from './schema/media'
+
 export * from './schema/user'
 
+export interface SimpleAttendee {
+	photo: Media
+	firstName: string
+	lastName: string
+}
+export interface AttendeeStore {
+	attendees: SimpleAttendee[]
+	lastUpdate: string
+	hash: string
+	num: number
+}
