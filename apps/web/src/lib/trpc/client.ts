730cd07b6e41df99dd8ef5d0c7178fcaf7cc317b
Nicky Hajal <nhajal@gmail.com>
2024-04-01 13:56:31 +0200

    tweak: get/post helper

--- apps/web/src/lib/trpc/client.ts
+++ apps/web/src/lib/trpc/client.ts
@@ -1,7 +1,38 @@
-import type { Router } from '@matterloop/api'
 import superjson from 'superjson'
 import { createTRPCClient, type TRPCClientInit } from 'trpc-sveltekit'
 
+import type { Router } from '@matterloop/api'
+
 export function trpc(init?: TRPCClientInit) {
 	return createTRPCClient<Router>({ init, transformer: superjson })
+}
+
+// export function trpc(init?: TRPCClientInit) {
+// 	return createTRPCProxyClient<Router>({
+// 		transformer: superjson,
+// 		links: [
+// 			httpBatchLink({
+// 				url: `${API_HOST}/trpc`,
+// 				fetch(url, options) {
+// 					return fetch(url, {
+// 						...options,
+// 						credentials: 'include',
+// 					})
+// 				},
+// 			}),
+// 		],
+// 	})
+// }
+
+export const get = (path: string) => {
+	return fetch(`/${path}`, {
+		credentials: 'include',
+	})
+}
+export const post = (path: string, body: any) => {
+	return fetch(`/${path}`, {
+		method: 'POST',
+		body: JSON.stringify(body),
+		credentials: 'include',
+	})
 }
--- apps/web/src/lib/types.ts
+++ apps/web/src/lib/types.ts
@@ -1,2 +1,16 @@
 // export { userTable, signupSchema, createUserSchema, adminAddCompanyUserSchema } from '$lib/schema/user'
 // export { companySchema, type CompanySchemaType, type Company,  } from '$lib/schema/company'
+
+import type { Media, User } from '@matterloop/db'
+
+export interface SimpleAttendee {
+	photo: Media
+	firstName: string
+	lastName: string
+}
+export interface AttendeeStore {
+	attendees: SimpleAttendee[]
+	lastUpdate: string
+	hash: string
+	num: number
+}
