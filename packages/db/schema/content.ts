a11461fbf85f44db4416a9edb5c98c93bd8a10eb
Nicky Hajal <nhajal@gmail.com>
2024-04-01 12:05:44 +0200

    wip: errors and redis

--- packages/db/schema/content.ts
+++ packages/db/schema/content.ts
@@ -1,31 +1,37 @@
 import { relations, sql, type InferModel } from 'drizzle-orm'
-import { integer, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
+import { integer, pgTable, text, timestamp, uniqueIndex, uuid } from 'drizzle-orm/pg-core'
 import { createInsertSchema } from 'drizzle-zod'
+import { eventTable } from './event'
 
 export const contentTable = pgTable('content', {
 	id: uuid('id')
-		.default(sql`uuid_generate_v4()`)
+		.default(sql`extensions.uuid_generate_v4()`)
 		.primaryKey()
 		.notNull(),
 	userId: text('user_id'),
 	type: text('type'),
+	key: text('key'),
 	status: text('status'),
 	eventId: uuid('event_id'),
 	parentId: uuid('parent_id'),
 	title: text('title'),
+	ord: integer('ord'),
 	subtitle: text('subtitle'),
 	body: text('body'),
 	createdAt: timestamp('created_at', { withTimezone: true, mode: 'string' }).defaultNow(),
 	updatedAt: timestamp('updated_at', { withTimezone: true, mode: 'string' }).defaultNow(),
+}, (table) => {
+	return {
+		eventContentKeyId: uniqueIndex('event_id_key').on(table.eventId, table.key),
+	}
 })
 
-// export const mediaRelations = relations(mediaTable, ({ many, one }) => ({
-// 	user: many(eventUserTable),
-// 	venue: one(venueTable, {
-// 		fields: [eventTable.venueId],
-// 		references: [venueTable.id],
-// 	}),
-// }))
+export const contentRelations = relations(contentTable, ({ many, one }) => ({
+	venue: one(eventTable, {
+		fields: [contentTable.eventId],
+		references: [eventTable.id],
+	}),
+}))
 
 export const contentSchema = createInsertSchema(contentTable, {
 	// name: (schema) => schema.name.min(1).default(''),
