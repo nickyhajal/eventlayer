28c27ea3f1859b907459608bd73895efbbd61f66
Nicky Hajal <nhajal@gmail.com>
2024-04-01 13:57:49 +0200

    feat: login and signup

---
+++ apps/web/src/routes/(pwa)/login/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+import LoginForm from '$lib/components/LoginForm.svelte'
+import Screen from '$lib/components/Screen.svelte'
+import { getEventContext } from '$lib/state/getContexts'
+</script>
+
+<Screen title="" back="/">
+	<div class="wrap mx-auto mt-40 max-w-md p-4">
+		<LoginForm />
+	</div>
+</Screen>
---
+++ apps/web/src/routes/(pwa)/login/+server.ts
@@ -0,0 +1,45 @@
+// routes/login/+page.server.ts
+import { error, fail, json, redirect } from '@sveltejs/kit'
+import { z } from 'zod'
+
+import { lucia } from '@matterloop/api'
+import { db, eq, loginLinkTable } from '@matterloop/db'
+
+const schema = z.object({
+	code: z.string(),
+})
+
+export const GET = async ({ params }) => {
+	return json({ status: 'success' })
+}
+export const POST = async ({ request, cookies, locals }) => {
+	const formData = await request.json()
+	console.log('>>>>>>> LOGIN')
+	try {
+		const { code } = schema.parse(formData)
+		console.log('>>>>>>> CODE', code)
+		const link = await db.query.loginLinkTable.findFirst({
+			where: eq(loginLinkTable.publicId, code),
+		})
+		console.log(1)
+		if (!link) {
+			console.log(2)
+			return error(401, 'Invalid code')
+		}
+		console.log(3)
+		const session = await lucia.createSession(link.userId, {})
+		console.log(4, session)
+		const sessionCookie = lucia.createSessionCookie(session.id)
+		console.log(5, sessionCookie)
+		cookies.set(sessionCookie.name, sessionCookie.value, {
+			...sessionCookie.attributes,
+			path: '.',
+			sameSite: process.env.NODE_ENV === 'development' ? 'lax' : 'none',
+		})
+		return json({ status: 'success' })
+	} catch (e) {
+		console.log(e)
+		return error(401, e)
+	}
+	return json({ status: 'error' })
+}
