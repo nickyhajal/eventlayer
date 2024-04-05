e79da9ad8b5f8319b1ae7ea5c6020fe509094325
Nicky Hajal <nhajal@gmail.com>
2024-04-05 11:41:41 +0200

    chore: li imports

--- apps/web/src/routes/(pwa)/auth/li_callback/+server.ts
+++ apps/web/src/routes/(pwa)/auth/li_callback/+server.ts
@@ -1,26 +1,26 @@
 import { json, redirect } from '@sveltejs/kit'
 
-import { EventFns, LinkedInAuthorizer } from '@matterloop/api'
+// import { EventFns, LinkedInAuthorizer } from '@matterloop/api'
 
 export const GET = async (req) => {
 	// console.log(liClient)
-	const search = req.url.searchParams
-	console.log(search.get('code'), search.get('state'))
-	const res = new Response()
-	LinkedInAuthorizer.auth.getAccessToken(
-		res,
-		search.get('code'),
-		search.get('state'),
-		function (err, results) {
-			if (err) return console.error(err)
+	// const search = req.url.searchParams
+	// console.log(search.get('code'), search.get('state'))
+	// const res = new Response()
+	// LinkedInAuthorizer.auth.getAccessToken(
+	// 	res,
+	// 	search.get('code'),
+	// 	search.get('state'),
+	// 	function (err, results) {
+	// 		if (err) return console.error(err)
 
-			/**
-			 * Results have something like:
-			 * {"expires_in":5184000,"access_token":". . . ."}
-			 */
+	// 		/**
+	// 		 * Results have something like:
+	// 		 * {"expires_in":5184000,"access_token":". . . ."}
+	// 		 */
 
-			console.log(results)
-		},
-	)
+	// 		console.log(results)
+	// 	},
+	// )
 	return new Response('ok')
 }
--- apps/web/src/routes/(pwa)/auth/li_start/+server.ts
+++ apps/web/src/routes/(pwa)/auth/li_start/+server.ts
@@ -1,12 +1,12 @@
 import { json, redirect } from '@sveltejs/kit'
 
-import { EventFns, LinkedInAuthorizer } from '@matterloop/api'
+// import { EventFns, LinkedInAuthorizer } from '@matterloop/api'
 
 export const GET = async (req) => {
 	// console.log(liClient)
-	const res = await fetch(`https://linkedin.com/in/nickyhajal`)
-	const text = await res.text()
-	console.log(text)
+	// const res = await fetch(`https://linkedin.com/in/nickyhajal`)
+	// const text = await res.text()
+	// console.log(text)
 
 	return new Response('<pre>' + text + '</pre>')
 }
