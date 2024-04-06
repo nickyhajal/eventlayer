6879a30edd926268d89554dedd53596fb3cfff54
Nicky Hajal <nhajal@gmail.com>
2024-04-06 08:56:48 +0200

    test: welcmoe

--- apps/web/src/routes/(pwa)/welcome/+page.server.ts
+++ apps/web/src/routes/(pwa)/welcome/+page.server.ts
@@ -5,16 +5,21 @@ import { EventFns, FormFns, VenueFns } from '@matterloop/api'
 export const load = async (req) => {
 	const { locals, url, params } = req
 
+	console.log(1, locals.me)
 	if (!locals.me?.onboardFormId) {
+		console.log(2, 'red')
 		redirect(302, '/')
 	}
 	const formId = locals?.me?.onboardFormId
+	console.log(3, 'fo', formId)
 	if (formId) {
+		console.log(4, 'fo', formId)
 		const formFns = FormFns({ formId })
 		const form = await formFns.get()
 		const session = await formFns.getSessionForUser(locals.me.id)
 		return { form, session }
 	} else {
+		console.log(5, 'red')
 		redirect(302, '/')
 	}
 }
