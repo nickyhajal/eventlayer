53a4953cb01c2c9d5c6324c7efe4f9957f7cb241
Nicky Hajal <nhajal@gmail.com>
2024-04-08 19:55:33 +0200

    chore: remove consoles

--- apps/web/src/routes/(pwa)/login/+server.ts
+++ apps/web/src/routes/(pwa)/login/+server.ts
@@ -14,23 +14,16 @@ export const GET = async ({ params }) => {
 }
 export const POST = async ({ request, cookies, locals }) => {
 	const formData = await request.json()
-	console.log('>>>>>>> LOGIN')
 	try {
 		const { code } = schema.parse(formData)
-		console.log('>>>>>>> CODE', code)
 		const link = await db.query.loginLinkTable.findFirst({
 			where: eq(loginLinkTable.publicId, code),
 		})
-		console.log(1)
 		if (!link) {
-			console.log(2)
 			return error(401, 'Invalid code')
 		}
-		console.log(3)
 		const session = await lucia.createSession(link.userId, {})
-		console.log(4, session)
 		const sessionCookie = lucia.createSessionCookie(session.id)
-		console.log(5, sessionCookie)
 		cookies.set(sessionCookie.name, sessionCookie.value, {
 			...sessionCookie.attributes,
 			path: '/',
