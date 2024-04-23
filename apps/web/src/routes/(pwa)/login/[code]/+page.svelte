917b38cff1610daf5714ee199a92f422bacab4a1
Nicky Hajal <nhajal@gmail.com>
2024-04-23 13:39:27 +0200

    fix: code login service worker clear

--- apps/web/src/routes/(pwa)/login/[code]/+page.svelte
+++ apps/web/src/routes/(pwa)/login/[code]/+page.svelte
@@ -5,19 +5,22 @@ import Screen from '$lib/components/Screen.svelte'
 import { getEventContext } from '$lib/state/getContexts'
 
 export let data
+console.log('data', data, browser)
 if (browser && data.success) {
+	console.log('unregister')
 	navigator?.serviceWorker?.getRegistrations()?.then((registrations) => {
 		for (const registration of registrations) {
 			registration.unregister()
 		}
 	})
-	setTimeout(() => {
-		location.href = '/'
-	}, 10)
+	// setTimeout(() => {
+	// 	location.href = '/'
+	// }, 10)
 }
 </script>
 
 <Screen title="" back="/">
+	Logging in...
 	{#if data.error}
 		<div class="wrap mx-auto mt-40 max-w-md p-4">That code isn't valid</div>
 	{/if}
