c76d066b17fb58c8cc808c28f6c94cafce7aff3e
Nicky Hajal <nhajal@gmail.com>
2024-10-02 11:14:47 -0500

    no info for logged out

--- apps/web/src/routes/(pwa)/+page.server.ts
+++ apps/web/src/routes/(pwa)/+page.server.ts
@@ -9,12 +9,15 @@ export const load = async (req) => {
 		const eventFns = EventFns({ eventId: locals.event.id })
 		return {
 			upcoming: await eventFns.getNextEvents(),
-			info: await db.query.eventUserInfoTable.findMany({
-				where: and(
-					eq(eventUserInfoTable.eventId, locals.event.id),
-					eq(eventUserInfoTable.userId, locals.me.id),
-				),
-			}),
+			info:
+				locals.me?.id && locals.event?.id
+					? await db.query.eventUserInfoTable.findMany({
+							where: and(
+								eq(eventUserInfoTable.eventId, locals.event.id),
+								eq(eventUserInfoTable.userId, locals.me.id),
+							),
+						})
+					: [],
 		}
 	}
 	return {}
