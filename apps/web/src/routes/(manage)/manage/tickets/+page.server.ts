7f6909f983d2ed338bf37ed7fe5d538900acb58f
Nicky Hajal <nhajal@gmail.com>
2025-03-12 10:43:45 -0500

    Only unused tickets

--- apps/web/src/routes/(manage)/manage/tickets/+page.server.ts
+++ apps/web/src/routes/(manage)/manage/tickets/+page.server.ts
@@ -1,14 +1,14 @@
 // routes/login/+page.server.ts
 import { error, fail, redirect } from '@sveltejs/kit'
 
-import { db, eq, eventTicketTable } from '@matterloop/db'
+import { db, eq, eventTicketTable, isNull } from '@matterloop/db'
 
 export const load = async ({ locals }) => {
 	if (!locals.event.id) {
 		error(404, 'Event not found')
 	}
 	const tickets = await db.query.eventTicketTable.findMany({
-		where: eq(eventTicketTable.eventId, locals.event.id),
+		where: and(eq(eventTicketTable.eventId, locals.event.id), isNull(eventTicketTable.assignedTo)),
 		with: {
 			user: true,
 		},
