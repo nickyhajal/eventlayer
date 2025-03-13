75edd806ca9c1b073ce8dd78894d178760f4f8a9
Nicky Hajal <nhajal@gmail.com>
2025-03-12 20:39:14 -0500

    Fix login links

--- apps/web/src/routes/(pwa)/welcome/+page.server.ts
+++ apps/web/src/routes/(pwa)/welcome/+page.server.ts
@@ -1,12 +1,28 @@
 import { redirect } from '@sveltejs/kit'
 
 import { EventFns, FormFns, VenueFns } from '@matterloop/api'
+import { and, db, eq, eventTicketTable, isNull } from '@matterloop/db'
 
 export const load = async (req) => {
 	const { locals, url, params } = req
-
+	// console.log('locals', locals.me)
 	if (!locals.me?.onboardFormId) {
-		redirect(301, '/')
+		if (locals.me?.id) {
+			const tickets = await db.query.eventTicketTable.findMany({
+				where: and(
+					eq(eventTicketTable.userId, locals.me.id),
+					eq(eventTicketTable.eventId, locals.event.id),
+					isNull(eventTicketTable.assignedTo),
+				),
+			})
+			if (tickets.length > 0) {
+				redirect(301, `/welcome/${tickets[0].assignKey}`)
+			} else {
+				redirect(301, '/')
+			}
+		} else {
+			redirect(301, '/')
+		}
 	}
 	const formId = locals?.me?.onboardFormId
 	if (formId) {
