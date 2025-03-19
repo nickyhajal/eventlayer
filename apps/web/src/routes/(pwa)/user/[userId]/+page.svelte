3febcd6e9f4346c07ff663425cf06a62c8561d13
Nicky Hajal <nhajal@gmail.com>
2025-03-18 20:04:02 -0500

    Update profile

--- apps/web/src/routes/(pwa)/user/[userId]/+page.svelte
+++ apps/web/src/routes/(pwa)/user/[userId]/+page.svelte
@@ -3,12 +3,10 @@
 	import Screen from '$lib/components/Screen.svelte'
 	import Button from '$lib/components/ui/button/button.svelte'
 	import UserAvatar from '$lib/components/UserAvatar.svelte'
-	import VenueBlock from '$lib/components/VenueBlock.svelte'
-	import { getMeContext } from '$lib/state/getContexts'
+	import Pin from 'lucide-svelte/icons/map-pin'
 
-	import type { Event } from '@matterloop/db'
 	import { Markdown } from '@matterloop/ui'
-	import { capitalize, dayjs, getMediaUrl, startCase } from '@matterloop/util'
+	import { capitalize, dayjs, startCase } from '@matterloop/util'
 
 	export let data
 	$: user = data.user
@@ -41,6 +39,14 @@
 	// }
 	// $: openToWork = user?.info?.['seeking-job']?.value === '["yes"]'
 	// $: openToHire = user?.info?.['hiring']?.value === '["yes"]'
+	function info(key: string) {
+		return user?.info[key]?.value
+	}
+	$: title = [info('title'), info('location')].filter(Boolean).join(' - ')
+	const questions = [
+		{ label: `What's your super power?`, key: 'superpower' },
+		{ label: `Ask me about...`, key: 'ask-me' },
+	]
 </script>
 
 <Screen title={name} back="/speakers">
@@ -75,13 +81,21 @@
 			{/if}
 			<div class="flex flex-col text-center">
 				<div class="xs:text-3xl pb-0 text-2xl font-bold">{name}</div>
-				{#if user?.info.title}
-					<div class="xs:text-base pb-0 text-[0.9rem] font-semibold text-slate-700">
-						{user.info.title.value}
+				{#if info('title')}
+					<div
+						class="xs:text-base pb-0 text-[0.9rem] justify-center font-semibold text-slate-700 flex"
+					>
+						{info('title')}
+						{#if info('location')}
+							<span class="pl-3 flex items-center gap-0.5">
+								<Pin class="w-4 inline text-stone-600 fill-blue-100 stroke-blue-300" />
+								{info('location')}
+							</span>
+						{/if}
 					</div>
 				{/if}
-				{#if user?.info.company}
-					<div class="pb-0 text-sm font-medium text-slate-600">{user.info.company.value}</div>
+				{#if info('company')}
+					<div class="pb-0 text-sm font-medium text-slate-600">{info('company')}</div>
 				{/if}
 			</div>
 		</div>
@@ -90,34 +104,80 @@
 				{eventStr}
 			</div>
 		</div>
-		{#if user?.url || user?.info?.linkedin_url?.value}
-			<Button
-				href={user?.url || user?.info?.linkedin_url?.value}
-				target="_blank"
-				rel="noopener noreferrer"
-				class="relative mx-auto -mt-2 mb-10 block h-9 w-full max-w-sm rounded-lg bg-sky-700 text-center text-sm"
-			>
-				Connect on LinkedIn
-			</Button>
-		{/if}
-		{#if user.bio}
-			<div class="border-t border-slate-200 pb-8 pt-8"></div>
-		{/if}
-		<Markdown
-			data={user.bio || user.info?.bio?.value || ''}
-			class="text-lg font-medium text-slate-600"
-		/>
-		{#if user.proBio}
-			<Markdown data={user.proBio} class="border-t border-slate-200 pr-4 pt-8 text-slate-600" />
-		{/if}
-		{#if user?.info?.['speechTitle']?.value}
-			<div class="mt-1 pt-8">
-				<div class="mb-2 font-semibold">Talk Title</div>
-				<div class="w-fit text-sm font-medium text-slate-600">
-					{user?.info?.['speechTitle']?.value}
-				</div>
+		<div class="flex gap-4 flex-col">
+			<div class="flex flex-wrap justify-start gap-2 mb-4">
+				{#if info('site')}
+					<Button
+						href={info('site')}
+						target="_blank"
+						rel="noopener noreferrer"
+						class="relative mx-auto flex-grow block h-9 w-fit  rounded-lg bg-teal-700 text-center text-sm"
+					>
+						View their Site
+					</Button>
+				{/if}
+				{#if user?.info?.linkedin_url?.value}
+					<Button
+						href={user?.info?.linkedin_url?.value}
+						target="_blank"
+						rel="noopener noreferrer"
+						class="relative mx-auto flex-grow block h-9 w-fit  rounded-lg bg-sky-800 text-center text-sm"
+					>
+						Connect on LinkedIn
+					</Button>
+				{/if}
+				{#if user?.info?.ig_url?.value}
+					<Button
+						href={user?.info?.ig_url?.value}
+						target="_blank"
+						rel="noopener noreferrer"
+						class="relative mx-auto flex-grow block h-9 w-fit rounded-lg bg-purple-800 text-center text-sm"
+					>
+						Connect on Instagram
+					</Button>
+				{/if}
+				{#if user?.info?.fb_url?.value}
+					<Button
+						href={user?.info?.fb_url?.value}
+						target="_blank"
+						rel="noopener noreferrer"
+						class="relative mx-auto flex-grow block h-9 w-fit rounded-lg bg-sky-700 text-center text-sm"
+					>
+						Connect on Facebook
+					</Button>
+				{/if}
 			</div>
-		{/if}
+			{#if user.bio}
+				<div class="border-t border-slate-200 pb-8 pt-8"></div>
+			{/if}
+			<Markdown
+				data={user.bio || user.info?.bio?.value || ''}
+				class="text-lg font-medium text-slate-600 pb-4 border-b border-slate-200"
+			/>
+			{#if user.proBio}
+				<Markdown data={user.proBio} class="border-t border-slate-200 pr-4 pt-8 text-slate-600" />
+			{/if}
+			<div class="mt-10">
+				{#each questions as { label, key }}
+					{#if info(key)}
+						<div class="pb-8">
+							<h3 class="text-xl font-semibold">{label}</h3>
+							<div class="w-fit text-base font-medium text-slate-600">
+								{info(key)}
+							</div>
+						</div>
+					{/if}
+				{/each}
+			</div>
+			{#if user?.info?.['speechTitle']?.value}
+				<div class="mt-1 pt-8">
+					<div class="mb-2 font-semibold">Talk Title</div>
+					<div class="w-fit text-sm font-medium text-slate-600">
+						{user?.info?.['speechTitle']?.value}
+					</div>
+				</div>
+			{/if}
+		</div>
 		{#if user?.info?.['traveling-from']?.value?.length}
 			<div class="mt-1 pt-8">
 				<div class="mb-2 font-semibold">Traveling From:</div>
@@ -128,11 +188,11 @@
 				</div>
 			</div>
 		{/if}
-		{#if user?.info?.['why-attending']?.value?.length > 2}
+		{#if user?.info?.['why']?.value?.length > 2}
 			<div class="mt-1 pt-8">
 				<div class="mb-2 font-semibold">Coming to Wings in order to:</div>
 				<div class="flex flex-col gap-1">
-					{#each JSON.parse(user?.info?.['why-attending']?.value) as key}
+					{#each JSON.parse(user?.info?.['why']?.value) as key}
 						{#if why[key]}
 							<div
 								class="w-fit rounded-md border border-slate-200/50 px-2 py-1 text-sm font-medium text-slate-600"
