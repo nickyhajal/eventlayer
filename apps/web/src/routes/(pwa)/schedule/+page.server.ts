fae5adb52fcca77b77ac484844dbc2b69adf6615
Nicky Hajal <nhajal@gmail.com>
2025-03-18 20:02:55 -0500

    Show program events on schedule

--- apps/web/src/routes/(pwa)/schedule/+page.server.ts
+++ apps/web/src/routes/(pwa)/schedule/+page.server.ts
@@ -4,7 +4,7 @@ import { orderBy } from '@matterloop/util'
 export const load = async (req) => {
 	const { locals, url } = req
 	const eventFns = EventFns({ eventId: locals.event.id })
-	const events = await eventFns.getEvents({ eventFor: 'all' })
+	const events = await eventFns.getEvents({ type: 'program' })
 	const myEvents = await eventFns.getUserEvents(locals.me)
 	return {
 		events: orderBy([...events, ...myEvents], ['startsAt']),
