1e32c026abc845bc90f71a86eede2cb7c46f5c59
Nicky Hajal <nhajal@gmail.com>
2025-03-20 22:22:21 -0500

    meetup listing

---
+++ apps/web/src/routes/(pwa)/meetups/+page.server.ts
@@ -0,0 +1,12 @@
+import { EventFns } from '@matterloop/api'
+import { orderBy } from '@matterloop/util'
+
+export const load = async (req) => {
+	const { locals, url } = req
+	const eventFns = EventFns({ eventId: locals.event.id })
+	const events = await eventFns.getEvents({ type: 'meetup' })
+	const myEvents = await eventFns.getUserEvents(locals.me)
+	return {
+		events: orderBy([...events, ...myEvents], ['startsAt']),
+	}
+}
