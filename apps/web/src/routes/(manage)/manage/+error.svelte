7f67f01685d2d0b2afb76a9c47e8687dc1fdeba2
Nicky Hajal <nhajal@gmail.com>
2025-12-16 18:36:16 -0600

    Fix userid

--- apps/web/src/routes/(manage)/manage/+error.svelte
+++ apps/web/src/routes/(manage)/manage/+error.svelte
@@ -1,26 +1,31 @@
 <script>
-	import { ExclamationTriangle } from '@steeze-ui/heroicons'
-	import { page } from '$app/stores'
+  import { ExclamationTriangle } from "@steeze-ui/heroicons";
+  import { page } from "$app/stores";
 
-	import { HeroIcon } from '@matterloop/ui'
+  import { HeroIcon } from "@matterloop/ui";
 </script>
 
 {#if $page.error}
-	<div class="m-8 max-w-4xl border-2 border-yellow-500/10 bg-yellow-50 text-slate-700">
-		<div
-			class="flex items-center gap-3 border-b border-yellow-500/20 bg-white/60 px-12 py-6 text-base font-semibold text-yellow-600/90"
-		>
-			<HeroIcon src={ExclamationTriangle} class="mt-1 h-6 w-6 text-yellow-500/70" theme="solid" /> There
-			was an issue
-		</div>
-		<div class="ml-1.5 px-20 py-12">
-			<div class="text-lg font-semibold text-slate-600">
-				{$page.error.message}
-			</div>
-			<div class="w-9/12 pt-1 text-sm font-medium text-slate-500">
-				Our team has been notified of this error. Contact support@reportnorth.com if you need
-				assistance.
-			</div>
-		</div>
-	</div>
+  <div
+    class="m-8 max-w-4xl border-2 border-yellow-500/10 bg-yellow-50 text-slate-700"
+  >
+    <div
+      class="flex items-center gap-3 border-b border-yellow-500/20 bg-white/60 px-12 py-6 text-base font-semibold text-yellow-600/90"
+    >
+      <HeroIcon
+        src={ExclamationTriangle}
+        class="mt-1 h-6 w-6 text-yellow-500/70"
+        theme="solid"
+      /> There was an issue
+    </div>
+    <div class="ml-1.5 px-20 py-12">
+      <div class="text-lg font-semibold text-slate-600">
+        {$page.error.message}
+      </div>
+      <div class="w-9/12 pt-1 text-sm font-medium text-slate-500">
+        Our team has been notified of this error. Contact support@eventlayer.co
+        if you need assistance.
+      </div>
+    </div>
+  </div>
 {/if}
--- apps/web/src/routes/(manage)/manage/people/[userId]/+page.server.ts
+++ apps/web/src/routes/(manage)/manage/people/[userId]/+page.server.ts
@@ -1,26 +1,33 @@
 // routes/login/+page.server.ts
-import { error, fail, redirect } from '@sveltejs/kit'
-import QRCode from 'qrcode'
-import { message, setError, superValidate } from 'sveltekit-superforms/server'
-import { z } from 'zod'
+import { error, fail, redirect } from "@sveltejs/kit";
+import QRCode from "qrcode";
+import { message, setError, superValidate } from "sveltekit-superforms/server";
+import { z } from "zod";
 
-import { EventFns, VenueFns } from '@matterloop/api'
-import { and, db, desc, eq, eventTicketTable, loginLinkTable } from '@matterloop/db'
-import { ActiveLoginLink } from '@matterloop/api/src/models/ActiveLoginLink'
+import { EventFns, VenueFns } from "@matterloop/api";
+import {
+  and,
+  db,
+  desc,
+  eq,
+  eventTicketTable,
+  loginLinkTable,
+} from "@matterloop/db";
+import { ActiveLoginLink } from "@matterloop/api/src/models/ActiveLoginLink";
 
 const schema = z.object({
   email: z.string().email(),
   password: z.string(),
-})
+});
 
 export const load = async ({ locals, params, url }) => {
   if (!params.userId) {
-    return error(404, 'No user id')
+    return error(404, "No user id");
   }
-  const eventFns = EventFns({ eventId: locals.event.id })
-  const user = await eventFns.getUser(params.userId)
+  const eventFns = EventFns({ eventId: locals.event.id });
+  const user = await eventFns.getUser(params.userId);
   if (!user) {
-    return error(404, 'User not found')
+    return error(404, "User not found");
   }
 
   const ticket = await db.query.eventTicketTable.findFirst({
@@ -31,19 +38,25 @@ export const load = async ({ locals, params, url }) => {
     with: {
       user: true,
     },
-  })
+  });
 
-  let login_link = null
+  let login_link = null;
   const login_links = await db.query.loginLinkTable.findMany({
     where: eq(loginLinkTable.userId, user.userId),
     orderBy: desc(loginLinkTable.createdAt),
-  })
+  });
   if (login_links.length) {
-    login_link = ActiveLoginLink.getUrl({ loginLink: login_links[0], event: locals.event })
+    login_link = ActiveLoginLink.getUrl({
+      loginLink: login_links[0],
+      event: locals.event,
+    });
   }
 
-  const qrcode = await QRCode.toDataURL(`${url.protocol}://${url.host}/user/${user.id}`, {
-    width: 320,
-  })
-  return { user, qrcode, ticket, login_link: login_link.url }
-}
+  const qrcode = await QRCode.toDataURL(
+    `${url.protocol}://${url.host}/user/${user.id}`,
+    {
+      width: 320,
+    },
+  );
+  return { user, qrcode, ticket, login_link: login_link?.url };
+};
