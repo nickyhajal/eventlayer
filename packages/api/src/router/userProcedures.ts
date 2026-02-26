161d7c5316ac5c31baf512a5a66d3d2af15e8db5
Nicky Hajal <nhajal@gmail.com>
2026-02-26 12:54:48 -0600

    Update email and onboarding

--- packages/api/src/router/userProcedures.ts
+++ packages/api/src/router/userProcedures.ts
@@ -90,18 +90,18 @@ export async function sendWelcomeEmail(user: User, event: Event, eventUser: Even
   if (url) {
     const res = await mailer.send({
       to: user?.email ?? '',
-      subject: `Action Required: Get Access to the Gathering Event App`,
+      subject: `Action Required: Get Access to the NeuroDiversion 26 App`,
       event: event,
       more_params: {
         body: `Hey ${user?.firstName},
-								<br><br>We’re excited to have you join us for the Innovation Network Gathering to shape the future of human-centric clinical trials.
-								<br><br>Instead of a one-time-use printed program, we’ve developed a helpful event app to guide you through all things related to the event.
-								<br><br>With the Gathering Event app you’ll be able to:
+								<br><br>We’re excited to have you join us for NeuroDiversion 2026!
+								<br><br>We’ve prepared a helpful event app to guide you through all things related to the event.
+								<br><br>You’ll be able to:
 								<ul>
 								<li>View a full detailed schedule</li>
-								<li>RSVP for a lunch option on Day 1</li>
+								<li>Browse and RSVP to Meetups</li>
 								<li>Access an attendee list including information about our speakers</li>
-								<li>Connect with our amazing Partners</li>
+								<li>Discover our Expo Partipicants</li>
 								<li>And more...</li>
 								</ul>
 								<br><br>To gain access, click the link below and set up your account.
