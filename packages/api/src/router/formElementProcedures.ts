0c6aa0649b3a4cceb6042d92de86414415d2d7e5
Nicky Hajal <nhajal@gmail.com>
2026-03-14 04:44:50 -0500

    update page on order

--- packages/api/src/router/formElementProcedures.ts
+++ packages/api/src/router/formElementProcedures.ts
@@ -4,7 +4,8 @@ import { z } from 'zod'
 import { db, eq, formElementSchema, formElementTable, inArray } from '@matterloop/db'
 import { groupBy } from '@matterloop/util'
 
-import { procedureWithContext, TrpcContext, verifyMe } from '../procedureWithContext'
+import { procedureWithContext, verifyMe } from '../procedureWithContext'
+import type { TrpcContext } from '../procedureWithContext'
 
 const t = initTRPC.context<TrpcContext>().create()
 
@@ -16,7 +17,7 @@ export const formElementProcedures = t.router({
   upsert: procedureWithContext
     // .use(verifyMe())
     .input(formElementSchema)
-    .mutation(async ({ ctx, input }) => {
+    .mutation(async ({ input }) => {
       const { createdAt, id } = input
       if (!createdAt) {
         const newForm = await db.insert(formElementTable).values(input).returning()
@@ -36,7 +37,7 @@ export const formElementProcedures = t.router({
   delete: procedureWithContext
     .use(verifyMe())
     .input(deleteSchema)
-    .mutation(async ({ ctx, input }) => {
+    .mutation(async ({ input }) => {
       await db.delete(formElementTable).where(eq(formElementTable.id, input.id))
       return true
     }),
@@ -49,29 +50,35 @@ export const formElementProcedures = t.router({
           z.object({
             id: z.string(),
             ord: z.number(),
+            page: z.number().optional(),
           }),
         ),
       }),
     )
-    .mutation(async ({ ctx, input }) => {
+    .mutation(async ({ input }) => {
       try {
         const { changes } = input
         await Promise.all(
-          changes.map(async ({ id, ord }) => {
+          changes.map(async ({ id, ord, page }) => {
             const children = await db.query.formElementTable.findMany({
               where: eq(formElementTable.formElementId, id),
             })
             const byOrder = groupBy(children, 'ord')
-            Object.entries(byOrder).forEach(async ([order, elements]) => {
-              const bits = `${order}`.split('.')
-              const newOrd = `${ord}.${bits[1]}`
-              const ids = elements.map(({ id }) => id)
-              await db
-                .update(formElementTable)
-                .set({ ord: +newOrd })
-                .where(inArray(formElementTable.id, ids))
-            })
-            return db.update(formElementTable).set({ ord }).where(eq(formElementTable.id, id))
+            await Promise.all(
+              Object.entries(byOrder).map(async ([order, elements]) => {
+                const bits = `${order}`.split('.')
+                const newOrd = `${ord}.${bits[1]}`
+                const ids = elements.map(({ id }) => id)
+                await db
+                  .update(formElementTable)
+                  .set({ ord: +newOrd, ...(page === undefined ? {} : { page }) })
+                  .where(inArray(formElementTable.id, ids))
+              }),
+            )
+            return db
+              .update(formElementTable)
+              .set({ ord, ...(page === undefined ? {} : { page }) })
+              .where(eq(formElementTable.id, id))
           }),
         )
         return new Response('Success', { status: 200 })
