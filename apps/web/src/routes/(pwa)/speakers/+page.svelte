d2b7a39c6651ce4cd4da6a0347eba92cf9c8e789
Nicky Hajal <nhajal@gmail.com>
2026-03-19 21:13:13 -0500

    speaker padding

--- apps/web/src/routes/(pwa)/speakers/+page.svelte
+++ apps/web/src/routes/(pwa)/speakers/+page.svelte
@@ -1,88 +1,92 @@
 <script lang="ts">
-	import Screen from '$lib/components/Screen.svelte'
-	import { getMeContext } from '$lib/state/getContexts'
+  import Screen from "$lib/components/Screen.svelte";
+  import { getMeContext } from "$lib/state/getContexts";
 
-	import { getMediaUrl, orderBy, startCase } from '@matterloop/util'
+  import { getMediaUrl, orderBy, startCase } from "@matterloop/util";
 
-	export let data
-	$: users = orderBy(data.users, ['mainEventUser.type'])
-	const me = getMeContext()
-	const typeOptions = [
-		{ label: 'Speakers', value: 'all' },
-		// { label: 'Speaker', value: 'speaker' },
-		// { label: 'Main Stage Speakers', value: 'main-stage-speaker' },
-		// { label: 'On Stage Hosts', value: 'on-stage-host' },
-		// { label: 'Facilitator', value: 'attendee' },
-	]
-	let showType = 'all'
-	const vals = {
-		'main-stage-speaker': 1,
-		'on-stage-host': 2,
-		// facilitator: 3,
-	}
-	$: ordered = orderBy(
-		users.map((user) => user),
-		[({ mainEventUser }) => vals[mainEventUser.type] || Infinity],
-		['asc'],
-	)
+  export let data;
+  $: users = orderBy(data.users, ["mainEventUser.type"]);
+  const me = getMeContext();
+  const typeOptions = [
+    { label: "Speakers", value: "all" },
+    // { label: 'Speaker', value: 'speaker' },
+    // { label: 'Main Stage Speakers', value: 'main-stage-speaker' },
+    // { label: 'On Stage Hosts', value: 'on-stage-host' },
+    // { label: 'Facilitator', value: 'attendee' },
+  ];
+  let showType = "all";
+  const vals = {
+    "main-stage-speaker": 1,
+    "on-stage-host": 2,
+    // facilitator: 3,
+  };
+  $: ordered = orderBy(
+    users.map((user) => user),
+    [({ mainEventUser }) => vals[mainEventUser.type] || Infinity],
+    ["asc"],
+  );
 </script>
 
 <Screen
-	title="Speakers"
-	bigTitle="Speakers"
-	titleSelectOptions={typeOptions}
-	bind:titleSelectValue={showType}
-	bodyClass="bg-slate-100"
+  title="Speakers"
+  bigTitle="Speakers"
+  titleSelectOptions={typeOptions}
+  bind:titleSelectValue={showType}
+  bodyClass="bg-slate-100"
 >
-	<div class="mx-auto -mt-2 max-w-7xl bg-slate-100">
-		<div class="mb-8 mt-16 max-w-2xl px-4 sm:px-0"></div>
-		<div class="mt-2 grid grid-cols-2 gap-1.5 py-2 md:grid-cols-3 md:gap-4">
-			{#each ordered.filter(({ mainEventUser, media }) => true || (media && (showType === 'all' || mainEventUser.type === showType))) as user}
-				{@const {
-					user: { id, firstName, lastName },
-					media,
-					mainEventUser,
-				} = user}
-				<a
-					href="/user/{mainEventUser.id}"
-					class="relative z-0 flex flex-col overflow-hidden rounded-xl border border-b-2 border-slate-400/20 bg-white p-0"
-				>
-					<div
-						class="mb-2 h-48 w-full rounded-t-lg bg-gradient-to-tl from-slate-100 to-slate-200 bg-cover bg-center"
-						style={media
-							? `background-image: url(${getMediaUrl(media, `w-256,h-256,fo-face,z-0.8`)})`
-							: ''}
-					></div>
-					<div class="px-2.5 pb-1.5 pt-1">
-						<div class="pb-1 text-xs font-semibold text-slate-500/80">
-							{startCase(mainEventUser.type?.replace('-speaker', ''))}
-						</div>
-						<div class=" truncate font-semibold text-[1.0.5rem] text-slate-800">
-							{firstName}
-							{lastName}
-						</div>
-						{#if mainEventUser.company || mainEventUser.title || user.title}
-							<div class="truncate pb-2.5 text-xs font-semibold text-slate-500">
-								{#if mainEventUser.company}
-									{mainEventUser.title}, {mainEventUser.company}
-								{:else if user.company}
-									{mainEventUser.company}
-								{:else if user.title}
-									{mainEventUser.title}
-								{/if}
-							</div>
-						{/if}
-						{#if mainEventUser?.proBio}
-							<div class="mb-1.5 line-clamp-2 text-xs font-medium text-slate-500">
-								{mainEventUser?.proBio}
-							</div>
-						{/if}
-						{#if !(mainEventUser?.proBio || mainEventUser.company || mainEventUser.title || user.title)}
-							<div class="pt-2"></div>
-						{/if}
-					</div>
-				</a>
-			{/each}
-		</div>
-	</div>
+  <div class="mx-auto -mt-2 max-w-7xl bg-slate-100">
+    <div class="mb-8 mt-16 max-w-2xl px-4 sm:px-0"></div>
+    <div
+      class="mt-2 pb-16 grid grid-cols-2 gap-1.5 py-2 md:grid-cols-3 md:gap-4"
+    >
+      {#each ordered.filter(({ mainEventUser, media }) => true || (media && (showType === "all" || mainEventUser.type === showType))) as user}
+        {@const {
+          user: { id, firstName, lastName },
+          media,
+          mainEventUser,
+        } = user}
+        <a
+          href="/user/{mainEventUser.id}"
+          class="relative z-0 flex flex-col overflow-hidden rounded-xl border border-b-2 border-slate-400/20 bg-white p-0"
+        >
+          <div
+            class="mb-2 h-48 w-full rounded-t-lg bg-gradient-to-tl from-slate-100 to-slate-200 bg-cover bg-center"
+            style={media
+              ? `background-image: url(${getMediaUrl(media, `w-256,h-256,fo-face,z-0.8`)})`
+              : ""}
+          ></div>
+          <div class="px-2.5 pb-1.5 pt-1">
+            <div class="pb-1 text-xs font-semibold text-slate-500/80">
+              {startCase(mainEventUser.type?.replace("-speaker", ""))}
+            </div>
+            <div class=" truncate font-semibold text-[1.0.5rem] text-slate-800">
+              {firstName}
+              {lastName}
+            </div>
+            {#if mainEventUser.company || mainEventUser.title || user.title}
+              <div class="truncate pb-2.5 text-xs font-semibold text-slate-500">
+                {#if mainEventUser.company}
+                  {mainEventUser.title}, {mainEventUser.company}
+                {:else if user.company}
+                  {mainEventUser.company}
+                {:else if user.title}
+                  {mainEventUser.title}
+                {/if}
+              </div>
+            {/if}
+            {#if mainEventUser?.proBio}
+              <div
+                class="mb-1.5 line-clamp-2 text-xs font-medium text-slate-500"
+              >
+                {mainEventUser?.proBio}
+              </div>
+            {/if}
+            {#if !(mainEventUser?.proBio || mainEventUser.company || mainEventUser.title || user.title)}
+              <div class="pt-2"></div>
+            {/if}
+          </div>
+        </a>
+      {/each}
+    </div>
+  </div>
 </Screen>
