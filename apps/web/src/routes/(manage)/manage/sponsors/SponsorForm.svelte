b5bfc2400853c6068c6413ed8a23947793e65c15
Nicky Hajal <nhajal@gmail.com>
2026-09-22 19:07:42 -0500

    fix: enable Whale Tank sponsor type selection

--- apps/web/src/routes/(manage)/manage/sponsors/SponsorForm.svelte
+++ apps/web/src/routes/(manage)/manage/sponsors/SponsorForm.svelte
@@ -42,7 +42,7 @@
 		{ value: 'sponsor', label: 'Sponsor' },
 		// { value: 'impact-partner', label: 'Impact Partner' },
 		// { value: 'organizing-partner', label: 'Organizing Partner' },
-		// { value: 'whale', label: 'Whale Tank' },
+		{ value: 'whale', label: 'Whale Tank' },
 	]
 	let sponsorExpoOptions = [
 		{ value: '0', label: 'No' },
